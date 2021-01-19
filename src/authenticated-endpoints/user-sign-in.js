5bfb3ca4a7683dcf6403782ecdf6d91204cb909e
joshingyou <github@joshyu.me>
2021-01-18 23:34:16 -0800

    Added firebase auth to user side

--- src/authenticated-endpoints/user-sign-in.js
+++ src/authenticated-endpoints/user-sign-in.js
@@ -6,12 +6,42 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+
 import PromptGraphics from "../widgets/prompt-graphics";
 import hello_img from "../hello.png";
+
+// Configure Firebase.
+const config = {
+  apiKey: "AIzaSyCGWQjgeoiIMJ9OWC055u7sowyKy9o3gCQ",
+  authDomain: "easy-waitlist.firebaseapp.com",
+  projectId: "easy-waitlist",
+  storageBucket: "easy-waitlist.appspot.com",
+  messagingSenderId: "681742178576",
+  appId: "1:681742178576:web:d2a5b0764aacd381bf6d95",
+  measurementId: "G-38CSHK03N0",
+};
+
+firebase.initializeApp(config);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/authenticated",
+
+  signInOptions: [firebase.auth.PhoneAuthProvider.PROVIDER_ID],
+};
 
 class UserSignIn extends Component {
   state = {};
   render() {
+    if (!firebase.apps.length) {
+    } else {
+      firebase.app(); // if already initialized, use that one
+    }
     return (
       <Container fluid className="my-3">
         <Row className="justify-content-md-center my-3">
@@ -21,28 +51,10 @@ class UserSignIn extends Component {
           />
           <Col>
             <Row className="my-3">
-              <h2>Enter your phone number to get started.</h2>
-            </Row>
-            <Row className="my-3">
-              <Form>
-                <Form.Group controlId="formPhoneNumber">
-                  <Form.Control type="tel" placeholder="Including leading +1" />
-                  <Form.Text className="text-muted">
-                    We'll never share your phone number with anyone else.
-                  </Form.Text>
-                </Form.Group>
-                <Button
-                  variant="primary"
-                  size="lg"
-                  type="submit"
-                  href="/authenticated"
-                >
-                  Text me
-                </Button>
-                <Button variant="primary" size="lg">
-                  Call me
-                </Button>
-              </Form>
+              <StyledFirebaseAuth
+                uiConfig={uiConfig}
+                firebaseAuth={firebase.auth()}
+              />
             </Row>
           </Col>
         </Row>
